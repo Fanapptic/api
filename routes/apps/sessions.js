9d2a3063e0d9c31a8b020500adb91679f46e209a
Braydon Batungbacal <me@braydonb.com>
2017-05-19 07:08:59 -0700

    Added initial /apps/:appId/sessions route.

---
+++ routes/apps/sessions.js
@@ -0,0 +1,74 @@
+/*
+ * Route: /apps/:appId/sessions/:appSessionId?
+ */
+
+const App = rootRequire('/models/App');
+const AppSession = rootRequire('/models/AppSession');
+const AppUser = rootRequire('/models/AppUser');
+
+const router = express.Router({
+  mergeParams: true,
+});
+
+/*
+ * GET
+ */
+
+router.get('/', (request,  response, next) => {
+  const userId = request.user.id;
+  const { appId, appSessionId } = request.params;
+
+  App.userHasPermission(appId, userId).then(() => {
+    if (appSessionId) {
+      AppSession.find({ where: { id: appSessionId, appId } });
+    } else {
+      AppSession.findAll({ where: { appId } });
+    }
+  }).then(result => {
+    response.success(result);
+  }).catch(next);
+});
+
+/*
+ * POST
+ */
+
+router.post('/', (request, response, next) => {
+  const { appId } = request.params;
+  const { appUserId } = request.body;
+
+  AppUser.find({ where: { id: appUserId, appId } }).then(appUser => {
+    if (!appUser) {
+      throw new Error('The user provided does not belong to the application provided.');
+    }
+
+    return AppSession.create({ appId, appUserId });
+  }).catch(next);
+});
+
+/*
+ * PUT
+ */
+
+router.put('/', (request, response, next) => {
+  const { appId, appSessionId } = request.params;
+  const { appUserId } = request.body;
+
+  AppSession.find({ where: { id: appSessionId, appId, appUserId } }).then(appSession => {
+    if (!appSession) {
+      throw new Error('app session put error');
+    }
+
+    appSession.duration = 1; // TODO: calculate this
+
+    return appSession.save();
+  }).then((appSession) => {
+    response.success(appSession);
+  }).catch(next);
+});
+
+/*
+ * Export
+ */
+
+module.exports = router;

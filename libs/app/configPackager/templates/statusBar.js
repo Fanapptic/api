45d2baa7fcb6ff4da63a3afec0dc66a288b1fd32
Braydon Batungbacal <me@braydonb.com>
2017-05-25 17:51:09 -0700

    Removed config packager in favor of a better solution.

--- libs/app/configPackager/templates/statusBar.js
+++
@@ -1,6 +0,0 @@
-const ConfigPackager = require('../configPackger');
-
-module.exports = {
-  barStyle: ConfigPackager.STRING.REQUIRED('default'),
-  hidden: ConfigPackager.BOOLEAN.REQUIRED(false),
-};

2c8144bf8409e2cdd27f0e40ae534d45a9f6eddc
Braydon Batungbacal <me@braydonb.com>
2018-01-17 15:32:24 -0800

    Updated shopify module and added configurableGrouping for look & feel.

--- appModules/Shopify/index.js
+++ appModules/Shopify/index.js
@@ -2,7 +2,9 @@
  * Module: Shopify
  */
 
-const { Module } = rootRequire('/libs/App/components');
+const { Module, ConfigurableGrouping } = rootRequire('/libs/App/components');
+const Configurable = rootRequire('/libs/App/Configurable');
+const { Option } = rootRequire('/libs/App/configurables');
 
 class Shopify extends Module {
   static get name() {
@@ -19,8 +21,28 @@ class Shopify extends Module {
         name: 'ion-bag',
         set: 'ion',
       },
-      moduleUrl: 'https://www.fiftythree.com/pencil',
+      moduleUrl: `${process.env.APP_MODULES_BASE_URL}/shopify/`,
     });
+
+    /*
+     * Look & Feed
+     */
+
+    const lookAndFeelGrouping = new ConfigurableGrouping({
+      name: 'lookAndFeel',
+      displayName: 'Look & Feel',
+      description: 'Tweak the visual aesthetics and layout of your shopify to better suit the overall design of your app.',
+    });
+
+    lookAndFeelGrouping.addConfigurable(new Option({
+      name: 'listMode',
+      displayName: 'List Mode',
+      description: 'Remove the padding around each product so they span the full width of the screen. Rounded borders are also removed.',
+      field: Configurable.FIELDS.SWITCH(),
+      defaultValue: false,
+    }));
+
+    this.addConfigurableGrouping(lookAndFeelGrouping);
   }
 }
 

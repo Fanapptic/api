c873ba85abd8ae8629b3758ae3174d7cd79c2ac1
Braydon Batungbacal <me@braydonb.com>
2018-01-24 22:17:12 -0800

    Added boilerplate for Gossip module. Removed Spotify module for MVP launch.

---
+++ appModules/Gossip/index.js
@@ -0,0 +1,27 @@
+/*
+ * Module: Gossip
+ */
+
+const { Module } = rootRequire('/libs/App/components');
+
+class Gossip extends Module {
+  static get name() {
+    return 'gossip';
+  }
+
+  constructor() {
+    super({
+      name: Gossip.name,
+      displayName: 'Gossip',
+      description: 'Give fans a change to ask, comment on, and upvote questions for you.',
+      brandingColor: '#435E8D',
+      defaultIcon: {
+        name: 'ion-chatboxes',
+        set: 'ion',
+      },
+      moduleUrl: `${process.env.APP_MODULES_BASE_URL}/gossip/`,
+    });
+  }
+}
+
+module.exports = Gossip;

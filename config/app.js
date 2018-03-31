6c61a2dcd56f8809d951c135f3aa53e7980d1cc2
Braydon Batungbacal <me@braydonb.com>
2018-03-30 18:18:46 -0700

    Reduced app version default to 2 parts. The third will be the bundleId / deployment Id.

--- config/app.js
+++ config/app.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   iconContentType: 'image/png',
   moduleLimit: 5,
-  version: '1.0.0',
+  version: '1.0',
   checklist: {
     brandingMarketing: {
       url: '/account/app/marketing',

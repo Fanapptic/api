5031bcae6a3baf58cbd683f76100986f13504be3
Braydon Batungbacal <me@braydonb.com>
2018-04-30 22:32:16 -0700

    Updated community module description.

--- appModules/community/index.js
+++ appModules/community/index.js
@@ -13,7 +13,7 @@ class Community extends Module {
     super({
       name: Community.name,
       displayName: 'Community',
-      description: 'Give your fans their own community to post content and interact with each other and you.',
+      description: 'Give your fans their own community to post content and chat.',
       brandingColor: '#435E8D',
       defaultIcon: 'entypo-chat',
       url: `${process.env.APP_MODULES_BASE_URL}/community/`,

994abd1fa5903ee977fd46ff3a54a03401ad85c2
Braydon Batungbacal <me@braydonb.com>
2018-06-28 03:26:16 -0700

    Added stripe payment handling through the /users/{userId}/charges endpoint.

--- setup/models.js
+++ setup/models.js
@@ -7,6 +7,7 @@ const AppSourceModel = rootRequire('/models/AppSource');
 const AppSourceContentModel = rootRequire('/models/AppSourceContent');
 const AppUserModel = rootRequire('/models/AppUser');
 const UserModel = rootRequire('/models/User');
+const UserChargeModel = rootRequire('/models/UserCharge');
 
 AppModel.hasMany(AppUserModel);
 AppModel.hasMany(AppDeviceModel);
@@ -25,5 +26,6 @@ AppUserModel.hasMany(AppDeviceSessionModel);
 AppUserModel.hasMany(AppFeedActivityModel);
 
 UserModel.hasMany(AppModel);
+UserModel.hasMany(UserChargeModel);
 
 module.exports = database.sync();

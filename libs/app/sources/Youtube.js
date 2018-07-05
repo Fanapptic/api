ca675caa80f3f73395e44fcc9b081c27de26c5d6
Braydon Batungbacal <me@braydonb.com>
2018-07-05 15:04:10 -0700

    Added correct youtube connection.

--- libs/app/sources/Youtube.js
+++ libs/app/sources/Youtube.js
@@ -5,7 +5,6 @@ const AppSourceContentModel = rootRequire('/models/AppSourceContent');
 const serverConfig = rootRequire('/config/server');
 const youtubeConfig = rootRequire('/config/sources/youtube');
 
-// need to get more than first 50 videos
 module.exports = class extends Source {
   connect() {
     return requestPromise.post({
@@ -20,6 +19,22 @@ module.exports = class extends Source {
     }).then(() => {
       return requestPromise.get({
         url: `${youtubeConfig.channelsUrl}?` +
+             'part=statistics' +
+             '&mine=true',
+        headers: {
+          Authorization: `Bearer ${this.appSource.accessToken}`,
+        },
+        json: true,
+      });
+    }).then(channels => {
+      const transaction = (this.sequelizeOptions) ? this.sequelizeOptions.transaction : null;
+
+      this.appSource.totalFans = channels.items[0].statistics.subscriberCount;
+
+      return this.appSource.save({ transaction });
+    }).then(() => {
+      return requestPromise.get({
+        url: `${youtubeConfig.channelsUrl}?` +
              'part=contentDetails' +
              '&mine=true',
         headers: {
@@ -30,29 +45,65 @@ module.exports = class extends Source {
     }).then(channels => {
       const uploadsPlaylistId = channels.items[0].contentDetails.relatedPlaylists.uploads;
 
-      return requestPromise.get({
-        url: `${youtubeConfig.playlistItemsUrl}?` +
-             `playlistId=${uploadsPlaylistId}` +
-             '&part=snippet' +
-             '&maxResults=50',
-        headers: {
-          Authorization: `Bearer ${this.appSource.accessToken}`,
-        },
-        json: true,
-      });
-    }).then(playlistItems => {
-      AppSourceContentModel.bulkCreate(playlistItems.items.map(playlistItem => {
-        return {
-          appId: this.appSource.appId,
-          appSourceId: this.appSource.id,
-          data: playlistItem,
-          publishedAt: playlistItem.snippet.publishedAt,
-        };
-      }));
+      const paginate = pageToken => {
+        let url = `${youtubeConfig.playlistItemsUrl}?` +
+                  `playlistId=${uploadsPlaylistId}` +
+                  '&part=snippet,status' +
+                  '&maxResults=50';
+
+        if (pageToken) {
+          url += `&pageToken=${pageToken}`;
+        }
+
+        requestPromise.get({
+          url,
+          headers: {
+            Authorization: `Bearer ${this.appSource.accessToken}`,
+          },
+          json: true,
+        }).then(playlistItems => {
+          playlistItems.items.forEach(playlistItem => {
+            const data = this.playlistItemToAppSourceContent(playlistItem);
+
+            if (playlistItem.status.privacyStatus !== 'public') {
+              return;
+            }
+
+            AppSourceContentModel.create(data).catch(e => {
+              console.log(e.message);
+              console.log(JSON.stringify(playlistItem));
+            });
+          });
+
+          if (playlistItems.nextPageToken) {
+            paginate(playlistItems.nextPageToken);
+          }
+        });
+      };
+
+      paginate();
     });
   }
 
   static handleWebhookRequest(request) {
 
+  }
+
+  /*
+   * Helpers
+   */
+
+  playlistItemToAppSourceContent(playlistItem) {
+    return {
+      appId: this.appSource.appId,
+      appSourceId: this.appSource.id,
+      iframe: {
+        url: `https://www.youtube.com/embed/${playlistItem.snippet.resourceId.videoId}`,
+      },
+      title: playlistItem.snippet.title,
+      description: playlistItem.snippet.description,
+      data: playlistItem,
+      publishedAt: playlistItem.snippet.publishedAt,
+    };
   }
 };

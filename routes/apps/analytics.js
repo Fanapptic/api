8ec8a255f9649f8187136e97c60e8de4466f8843
Braydon Batungbacal <me@braydonb.com>
2018-07-09 18:34:49 -0700

    Added analytics.

--- routes/apps/analytics.js
+++ routes/apps/analytics.js
@@ -2,6 +2,9 @@
  * Route: /apps/:appId/analytics
  */
 
+const AppDeviceModel = rootRequire('/models/AppDevice');
+const AppDeviceSessionModel = rootRequire('/models/AppDeviceSession');
+const AppFeedActivityModel = rootRequire('/models/AppFeedActivity');
 const userAuthorize = rootRequire('/middlewares/users/authorize');
 const appAuthorizeOwnership = rootRequire('/middlewares/apps/authorizeOwnership');
 
@@ -19,22 +22,163 @@ router.get('/', (request, response, next) => {
   const { appId } = request.params;
   let promises = [];
 
+  const dayAgoDate = new Date();
+  dayAgoDate.setDate(dayAgoDate.getDate() - 1);
+
+  const weekAgoDate = new Date();
+  weekAgoDate.setDate(weekAgoDate.getDate() - 7);
+
+  let weeklyDownloads = 0;
+  let totalDownloads = 0;
+  let dailyActiveUsers = 0;
+  let weeklyActiveUsers = 0;
+  let dailyUsage = 0;
+  let totalUsage = 0;
+  let dailyContentViews = 0;
+  let totalContentViews = 0;
+
+  // Weekly Downloads
+  promises.push(
+    AppDeviceModel.count({
+      where: {
+        appId,
+        createdAt: {
+          $gte: weekAgoDate,
+        },
+      },
+    }).then(_weeklyDownloads => weeklyDownloads = _weeklyDownloads)
+  );
+
+  // Total Downloads
+  promises.push(
+    AppDeviceModel.count({
+      where: { appId },
+    }).then(_totalDownloads => totalDownloads = _totalDownloads)
+  );
+
+  // Daily Active Users
+  promises.push(
+    AppDeviceSessionModel.count({
+      where: {
+        appId,
+        startedAt: {
+          $gte: dayAgoDate,
+        },
+      },
+      group: [ 'appDeviceId' ],
+    }).then(_dailyActiveUsers => dailyActiveUsers = _dailyActiveUsers)
+  );
+
+  // Weekly Active Users
+  promises.push(
+    AppDeviceSessionModel.count({
+      where: {
+        appId,
+        startedAt: {
+          $gte: weekAgoDate,
+        },
+      },
+      group: [ 'appDeviceId' ],
+    }).then(_weeklyActiveUsers => weeklyActiveUsers = _weeklyActiveUsers)
+  );
+
+  // Daily Usage
+  let dailyUsageSumStartedAt = 0;
+
+  promises.push(
+    AppDeviceSessionModel.sum('startedAt', {
+      where: {
+        appId,
+        startedAt: {
+          $gte: dayAgoDate,
+        },
+        endedAt: {
+          $ne: null,
+        },
+      },
+    }).then(_dailyUsageSumStartedAt => {
+      dailyUsageSumStartedAt = _dailyUsageSumStartedAt;
+
+      return AppDeviceSessionModel.sum('endedAt', {
+        where: {
+          appId,
+          startedAt: {
+            $gte: dayAgoDate,
+          },
+          endedAt: {
+            $ne: null,
+          },
+        },
+      });
+    }).then(dailyUsageSumEndedAt => {
+      dailyUsage =  dailyUsageSumEndedAt - dailyUsageSumStartedAt;
+    })
+  );
+
+  // Total Usage
+  let totalUsageSumStartedAt = 0;
+
+  promises.push(
+    AppDeviceSessionModel.sum('startedAt', {
+      where: {
+        appId,
+        endedAt: {
+          $ne: null,
+        },
+      },
+    }).then(_totalUsageSumStartedAt => {
+      totalUsageSumStartedAt = _totalUsageSumStartedAt;
+
+      return AppDeviceSessionModel.sum('endedAt', {
+        where: {
+          appId,
+          endedAt: {
+            $ne: null,
+          },
+        },
+      });
+    }).then(totalUsageSumEndedAt => {
+      dailyUsage =  totalUsageSumEndedAt - totalUsageSumStartedAt;
+    })
+  );
+
+  // Daily Content Views
+  promises.push(
+    AppFeedActivityModel.count({
+      where: {
+        appId,
+        createdAt: {
+          $gte: dayAgoDate,
+        },
+      },
+    }).then(_dailyContentViews => dailyContentViews = _dailyContentViews)
+  );
+
+  // Total Content Views
+  promises.push(
+    AppFeedActivityModel.count({
+      where: { appId },
+    }).then(_totalContentViews => totalContentViews = _totalContentViews)
+  );
+
   // Response
   Promise.all(promises).then(() => {
     response.success({
       downloads: {
-        current30Days: 0,
-        total: 0,
+        weeklyDownloads,
+        totalDownloads,
       },
       activeUsers: {
-        current30Days: 0,
+        dailyActiveUsers,
+        weeklyActiveUsers,
       },
       usage: {
-        current30Days: 0,
-        total: 0,
+        dailyUsage,
+        totalUsage,
       },
-      adRevenue: {
-        current30Days: 0,
+      contentViews: {
+        dailyContentViews,
+        totalContentViews,
       },
     });
   }).catch(next);
